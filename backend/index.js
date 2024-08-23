87a115e67cf93704f3f82de6685ab950f308d32e
vivekraj2704 <rajvivek2704@gmail.com>
2024-08-23 20:23:35 +0530

    most probably completed the backend part

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { createTodo } = require('./types');
+const { todo } = require('./db');
 const app = express(); 
 
 app.use(express.json());
 
-app.post('/todo', () => {
+app.post('/todo', async function(req, res) {
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload);
     if(!parsedPayload) {
@@ -13,14 +14,27 @@ app.post('/todo', () => {
         })
         return;
     }
-    //Put it in payload
+
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed: false
+    })
+
+    res.json({
+        msg: "todo created"
+    })
+    
 })
 
-app.get('/todos', () => {
-      
+app.get('/todos', async function(req, res) {
+      const todos = await todo.find({});
+      res.json({
+        todos
+      })
 })
 
-app.put('/completed', () => {
+app.put('/completed', async function(req, res) {
       const updatePayload = req.body;
       const parsedPayload = updateTodo.safeParse(updatePayload);
       if(!parsedPayload) {
@@ -29,6 +43,16 @@ app.put('/completed', () => {
         })
         return;
       }
+
+      await todo.update({
+        _id: req.body.id
+      }, {
+        completed: true
+      })
+
+      res.json({
+        msg: "your todo is marked as done"
+      })
 })
 
 app.listen(3000)
