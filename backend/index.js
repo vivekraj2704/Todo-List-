35a465b2882d7f313514c4d56a7589c31cfcc991
vivekraj2704 <rajvivek2704@gmail.com>
2024-08-23 20:03:34 +0530

    just before the mongodb part

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const { createTodo } = require('./types');
 const app = express(); 
 
 app.use(express.json());
 
 app.post('/todo', () => {
-      
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if(!parsedPayload) {
+        res.status(411).json ({
+            msg: "you sent the wrong inputs"
+        })
+        return;
+    }
+    //Put it in payload
 })
 
 app.get('/todos', () => {
@@ -12,7 +21,14 @@ app.get('/todos', () => {
 })
 
 app.put('/completed', () => {
-      
+      const updatePayload = req.body;
+      const parsedPayload = updateTodo.safeParse(updatePayload);
+      if(!parsedPayload) {
+        res.status(411).json({
+            msg: "you sent the wrong inputs"
+        })
+        return;
+      }
 })
 
 app.listen(3000)
