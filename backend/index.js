76f63a51af55b98f59d4925c79bead220e106584
vivekraj2704 <rajvivek2704@gmail.com>
2024-08-23 17:52:30 +0530

    just initialized backend part

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express(); 
+
+app.use(express.json());
+
+app.post('/todo', () => {
+      
+})
+
+app.get('/todos', () => {
+      
+})
+
+app.put('/completed', () => {
+      
+})
+
+app.listen(3000)
