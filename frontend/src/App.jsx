021137db5ca2b966cab21f9754d7d4c44d9005e3
vivekraj2704 <rajvivek2704@gmail.com>
2024-08-23 22:35:32 +0530

    started doing the basic frontend thing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import { CreateTodo } from './components/CreateTodo'
+import { Todos } from './components/Todos'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <CreateTodo></CreateTodo>
+      <Todos></Todos>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Todos.jsx
@@ -0,0 +1,8 @@
+
+export function Todos() {
+    return <div>
+        <h1>Got to Gym</h1>
+        <h2>You need to go to the gym</h2>
+        <button>Mark as done</button>
+    </div>
+}
